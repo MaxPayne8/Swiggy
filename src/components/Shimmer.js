b13c2a5b985f95707373f1711e5d25db1f1c57fe
MaxPayne8 <maxpayne0852.com>
2023-10-18 21:29:37 +0530

    shimmerUiUpdate

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -22,6 +22,7 @@ const Shimmer = () => {
       <div className="bg-slate-500 m-3 p-2 w-60 h-72 rounded-lg hover:cursor-pointer border-black border-4 hover:bg-orange-500 shadow-2xl"></div>
       <div className="bg-slate-500 m-3 p-2 w-60 rounded-lg hover:cursor-pointer border-black border-4 hover:bg-orange-500 shadow-2xl"></div>
       <div className="bg-slate-500 m-3 p-2 w-60 rounded-lg hover:cursor-pointer border-black border-4 hover:bg-orange-500 shadow-2xl"></div>
+      <div className="bg-slate-500 m-3 p-2 w-60 rounded-lg hover:cursor-pointer border-black border-4 hover:bg-orange-500 shadow-2xl"></div>
     </div>
   );
 };
